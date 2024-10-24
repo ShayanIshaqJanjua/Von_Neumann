11a1be3b92987c5bd541e95f227cfadf22d72898
Shayan Janjua <shayan.janjua@city.ac.uk>
2024-10-24 09:30:42 +0100

    Figured out binary shift for new system

--- src/what.java
+++ src/what.java
@@ -1,11 +1,10 @@
 public class what {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
-        int i =0;
-        while(i<10){
-            System.out.println(array[i]);
-            i++;
-        }
+        byte a = 0b0111;
+        byte b = 0b1010;
+        int c = a<<4 | b;
+        System.out.println(Integer.toBinaryString(c));
+
     }
 }
 
