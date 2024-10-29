dab468846f8dd325e8146d2b8a03735705cef01b
Shayan Janjua <shayan.janjua@city.ac.uk>
2024-10-29 10:44:49 +0000

    System returns individual processes

--- src/CPU.java
+++ src/CPU.java
@@ -9,6 +9,8 @@ public class CPU {
 
     public static Integer[] MainMemory = new Integer[200];
     public static HashMap<String, Integer> dataLocals = new HashMap<String, Integer>();
+    public static HashMap<String, Integer> branches = new HashMap<String, Integer>();
+    static HashMap<String, Integer> instructSet = new HashMap<String, Integer>(); // Holds the instruction set
     public int pc = 0;
     public int alu = 0;
     public int acc = 0;
@@ -18,13 +20,13 @@ public class CPU {
     public int cu = 0;
     public static void main(String[] args) {
         loadToMemory("base");
-        new CPU().Fetch();
+        //new CPU().Fetch();
     }
 
 
     private static void loadToMemory(String f) {
 
-        HashMap<String, Integer> instructSet = new HashMap<String, Integer>(); // Holds the instruction set
+
         instructSet.put("LDA", 0b0001);//Loads data from the data location given to the ACC
         instructSet.put("STA", 0b0010);//Stores data from the ACC to the given data location
         instructSet.put("ADD", 0b0011);//Adds the value from the given data location to the ACC
@@ -55,8 +57,11 @@ public class CPU {
                 if (temp[i].equals("OUT")) {
                 } else {
                     String[] s = temp[i].split(" "); // Lexical Analysis, split code into tokens
+                    if(!instructSet.containsKey(s[0])) {
+                        branches.put(s[0], i);
+                    }
                     if (s[0].equals("DAT")) {
-                        dataLocals.put(s[1], i + 1); // saves the name of the data location used with the location address
+                        dataLocals.put(s[1], i); // saves the name of the data location used with the location address
                     }
                 }
             }
@@ -64,8 +69,7 @@ public class CPU {
 
 
 
-        int opcode =0;//to store the opcode
-        int operand = 0;//to store the operand
+        int line = 0;
         int num = 0;
         for(String l : temp){//loops through every element of the temp array
             if(l == null){
@@ -74,53 +78,114 @@ public class CPU {
             else {
                 if (l.equals("OUT")) {
                     MainMemory[num] = 0b1111000000000;
-                } else {
-                    String[] s = l.split(" ");//Lexical Analysis, split code into tokens
-                    if (dataLocals.get(s[1]) != null) {//checks if a known data location is referenced
-                        operand = dataLocals.get(s[1]) | 0b100000000;//replaces the name of the location with the address in the code
-                    } else {
-                        try {
-                            operand = Integer.parseInt(s[1]);//adds the immediate value of the integer provided
-                        } catch (NumberFormatException e) {
-                            System.out.println("Error on line " + num + " relating to the data location reference.");
-                        }
-
-
+                }
+                else {
+                    String[] s = l.split(" ");
+                    if (branches.containsKey(s[0])) {
+                        MainMemory[num] = branchedInstruction(s);
                     }
-                    try {
-                        opcode = instructSet.get(s[0])<<9;//gets the integer value of the instruction from the instruction set and adds it to the opcode
-                    } catch (NullPointerException e) {
-                        System.out.println("Error on line " + num + " relating to the instruction spelling (Ensure all characters are CAPS).");
+                    else{
+                        MainMemory[num] = Instruction(s);
                     }
-                    MainMemory[num] = opcode | operand;//combines the opcode and operand and loads it into the main memory
-                    //increments the data address
                 }
             }
             num++;
         }
+
+
         for(int i = 0; i < MainMemory.length; i++) {
             if (MainMemory[i] != null) {
-                System.out.println(Integer.toBinaryString(MainMemory[i]));
+                System.out.println( i + ". " + Integer.toBinaryString(MainMemory[i]));
             }
         }
 
 
 
     }
+    public static int Instruction(String[] s) {
+        return 0;
+    }
+    public static int branchedInstruction(String[] strings) {
+        int opcode = 0;
+        try {
+            opcode = instructSet.get(strings[1])<<9;
+        }catch (Exception e) {
+            System.out.println("Error with instruction syntax on a branched line of code");
+        }
+        int operand = 0;
+        if(dataLocals.containsKey(strings[2])) {
+            operand = dataLocals.get(strings[2]) | 0b100000000;
+        }
+        else{
+            try{
+                operand = Integer.parseInt(strings[2]);
+            }
+            catch (Exception e) {
+                System.out.println("Error with data location syntax on a branched line of code");
+            }
+        }
+
+        return opcode |operand;
+    }
+
+    public void OldLoadToMemory() {
+        String[] s = l.split(" ");//Lexical Analysis, split code into tokens
+        if(branches.get(s[0])!= null){
+            branched = true;
+            if (dataLocals.get(s[2]) != null) {//checks if a known data location is referenced
+                operand = dataLocals.get(s[2]) | 0b100000000;//replaces the name of the location with the address in the code
+            } else {
+                try {
+                    operand = Integer.parseInt(s[2]);//adds the immediate value of the integer provided
+                } catch (NumberFormatException e) {
+                    System.out.println("Error on line " + num + " relating to the data location reference.");
+                }
+
+            }
+
+        }
+        if(branches.get(s[1]) != null){
+            System.out.println("why");
+            operand = branches.get(s[1]) | 0b100000000;
+        }else {
+            try {
+                operand = Integer.parseInt(s[1]);//adds the immediate value of the integer provided
+            } catch (NumberFormatException e) {
+                System.out.println("is this true");
+                System.out.println("Error on line " + num + " relating to the data location reference.");
+            }
+
+        }
+
+
+        try {
+            if(branched){
+                opcode = instructSet.get(s[1])<<9;
+                branched = false;
+            }
+            else{
+                opcode = instructSet.get(s[0])<<9;//gets the integer value of the instruction from the instruction set and adds it to the opcode
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Error on line " + num + " relating to the instruction spelling (Ensure all characters are CAPS).");
+        }
+        MainMemory[num] = opcode | operand;//combines the opcode and operand and loads it into the main memory
+
+    }
 
     public void Fetch(){
         mar = pc;
-        System.out.println("Address copied from Program Counter to memory address register.");
+        //      System.out.println("Address copied from Program Counter to memory address register.");
         if(MainMemory[mar] == null){
             return;
         }
         else{
             cir = MainMemory[mar];
-            System.out.println("Copied the instruction address from the ");
+            //System.out.println("Copied the instruction address from the ");
             cu = cir;
-            System.out.println("Instruction sent to Control Unit");
+            //System.out.println("Instruction sent to Control Unit");
             pc++;
-            System.out.println("Program counter incremented.");
+            //System.out.println("Program counter incremented.");
             Decode();
         }
 
@@ -133,10 +198,10 @@ public class CPU {
             return;
         }
         else {
-            System.out.println("Instruction decoded.");
+            //System.out.println("Instruction decoded.");
             cir = opcode;
             mdr = operand;
-            System.out.println("Instruction sent to Current Instruction Register.");
+            //System.out.println("Instruction sent to Current Instruction Register.");
             Execute();
         }
     }
@@ -168,6 +233,7 @@ public class CPU {
                 }
                 alu +=acc;
                 acc = alu;
+                break;
             case 0b0100:
                 alu = acc;
                 if((mdr&0b100000000) == 0b100000000 ){
@@ -198,5 +264,9 @@ public class CPU {
 //PROBLEM: CANNOT IMMEDIATE ACCESS AS CPU ASSUMES DIRECT ADDRESSING ALWAYS
 //SOLUTION: A WAY OF INDICATING A DIFFERENCE BETWEEN DATA LOCATIONS AND IMMEDIATE DATA
 //POSSIBLE SOLUTION: USE A CHECK TO SEE IF NUMBER IS A KNOWN DATA LOCATION
-//POSSIBLE SOLUTION: IMMEDIATE DATA IS NEGATIVE NUMBERS, DATA LOCATION ARE POSITIVE FC,FDDDDDDDDDDDDDINT
-//LIKELY SOLUTION: CONVERT INTEGER DATA LOCATIONS AND INSTRUCTIONS INTO BINARY, USE 1 BIT TO INDICATE DIRECt OR IMMEDIATE ADDRESSING
+//POSSIBLE SOLUTION: IMMEDIATE DATA IS NEGATIVE NUMBERS, DATA LOCATION ARE POSITIVE
+//LIKELY SOLUTION [WORKED] : CONVERT INTEGER DATA LOCATIONS AND INSTRUCTIONS INTO BINARY, USE 1 BIT TO INDICATE DIRECt OR IMMEDIATE ADDRESSING
+
+//PROBLEM: CANNOT READ BRANCH STATEMENTS OR BRANCHED AREAS OF CODE
+//SOLUTION: INTEGRATE SOME WAY OF CHECKING IF A LINE OF CODE CONTAINS A BRANCH
+//LIKELY SOLUTION: SPLIT THE LOAD TO MEMORY FUNCTION INTO SEPERATE FUNCTION WITH SOME DESIGNED FOR BRANCHED AND SOME FOR UNBRANCHED CODE
