93ade84708eafde6b9f045f9a1c7eba9fee429bf
Shayan Janjua <shayan.janjua@city.ac.uk>
2024-10-30 16:35:13 +0000

    Branching now fully works, also implemented a full adder for addition to the acc rather than just using + operator. Also implemented 2s compliment for subtraction. Next: GUI

--- src/CPU.java
+++ src/CPU.java
@@ -7,13 +7,13 @@ import java.util.*;
 
 public class CPU {
 
-    public static Integer[] MainMemory = new Integer[200];
+    public static int[] MainMemory = new int[200];
     public static HashMap<String, Integer> dataLocals = new HashMap<String, Integer>();
     public static HashMap<String, Integer> branches = new HashMap<String, Integer>();
     static HashMap<String, Integer> instructSet = new HashMap<String, Integer>(); // Holds the instruction set
     public int pc = 0;
-    public int alu = 0;
-    public int acc = 0;
+    public byte alu = 0;
+    public byte acc = 0;
     public int cir = 0;
     public int mdr = 0;
     public int mar = 0;
@@ -39,6 +39,7 @@ public class CPU {
 
 
         InputStream inputStream = CPU.class.getClassLoader().getResourceAsStream("base.txt");
+        assert inputStream != null;
         Scanner readLine = new Scanner(inputStream);
 
         String[] temp = new String[MainMemory.length];//initialises string array to hold lines of code
@@ -57,10 +58,10 @@ public class CPU {
                 if (temp[i].equals("OUT")) {
                 } else {
                     String[] s = temp[i].split(" "); // Lexical Analysis, split code into tokens
-                    if(!instructSet.containsKey(s[0])) {
-                        branches.put(s[0], i);
+                    if (!instructSet.containsKey(s[0])) {
+                        branches.put(s[0], i );
                     }
-                    if (s[0].equals("DAT") || s[0].equals("BRP") || s[0].equals("BRZ") || s[0].equals("BRA")) {
+                    if (s[0].equals("DAT")) {
                         dataLocals.put(s[1], i); // saves the name of the data location used with the location address
                     }
                 }
@@ -68,12 +69,11 @@ public class CPU {
         }
 
 
-
         int line = 0;
         int num = 0;
         for(String l : temp){//loops through every element of the temp array
             if(l == null){
-
+                return;
             }
             else {
                 if (l.equals("OUT")) {
@@ -81,11 +81,14 @@ public class CPU {
                 }
                 else {
                     String[] s = l.split(" ");
-                    if (branches.containsKey(s[0])) {
-                        MainMemory[num] = branchedInstruction(s);
-                    }
-                    else{
-                        MainMemory[num] = Instruction(s);
+                    if(s[1].equals("OUT")){
+                        MainMemory[num] = 0b1111000000000;
+                    }else {
+                        if (branches.containsKey(s[0])) {
+                            MainMemory[num] = branchedInstruction(s);
+                        } else {
+                            MainMemory[num] = Instruction(s);
+                        }
                     }
                 }
             }
@@ -94,7 +97,7 @@ public class CPU {
 
 
         for(int i = 0; i < MainMemory.length; i++) {
-            if (MainMemory[i] != null) {
+            if (MainMemory[i] == 0) {
                 System.out.println( i + ". " + Integer.toBinaryString(MainMemory[i]));
             }
         }
@@ -105,14 +108,15 @@ public class CPU {
     public static int Instruction(String[] s) {
         int opcode = instructSet.get(s[0])<<9;
         int operand =0;
-        if (dataLocals.keySet().contains(s[1])) {
+        if (dataLocals.containsKey(s[1])) {
             operand = dataLocals.get(s[1]) | 0b100000000;
-        }
-        else{
+        } else if (branches.containsKey(s[1])) {
+            operand= branches.get(s[1]);
+        } else{
             try{
-                System.out.println(s[1]);
                 operand = Integer.parseInt(s[1]);
             }catch(Exception e){
+                System.out.println(s[1]);
                 System.out.println("Error with data Location.");
             }
         }
@@ -120,6 +124,10 @@ public class CPU {
     }
     public static int branchedInstruction(String[] strings) {
         int opcode = 0;
+        if(strings[1].equals("OUT")) {
+            return 0b1111000000000;
+        }
+
         try {
             opcode = instructSet.get(strings[1])<<9;
         }catch (Exception e) {
@@ -189,7 +197,7 @@ public class CPU {
     public void Fetch(){
         mar = pc;
         //      System.out.println("Address copied from Program Counter to memory address register.");
-        if(MainMemory[mar] == null){
+        if(MainMemory[mar] == 0){
             return;
         }
         else{
@@ -223,48 +231,55 @@ public class CPU {
         switch (cir){
             case 0b0001:
                 if((mdr&0b100000000) == 0b100000000 ){
-                    acc = MainMemory[mdr&0b011111111];
-                    System.out.println("Data at location " + mdr + " Loaded into Accumulator.");
+                    acc = (byte) MainMemory[mdr&0b011111111];
+                    System.out.println("Data at location " + (mdr&0b011111111) + " Loaded into Accumulator.");
                 }
-                else{
-                    acc = mdr&0b011111111;
-                    System.out.println("Value " + mdr + " loaded into accumulator.");
+                else{                    acc = (byte) (mdr&0b011111111);
+                    System.out.println("Value " + (mdr&0b011111111) + " loaded into accumulator.");
                 }
                 break;
             case 0b0010:
                 MainMemory[mdr & 0b011111111] = acc;
-                System.out.println("Stored data from accumulator to memory location " + mdr);
+                System.out.println("Stored data from accumulator to memory location " + (mdr&0b011111111));
                 break;
             case 0b0011:
                 if((mdr&0b100000000) == 0b100000000 ){
-                    alu = MainMemory[mdr&0b011111111];
-                    System.out.println("Added data from memory location " + mdr + " to accumulator.");
+                    alu = (byte) MainMemory[mdr&0b011111111];
+                    System.out.println("Added data from memory location " + (mdr&0b011111111) + " to accumulator.");
                 }
                 else{
-                    alu = mdr&0b011111111;
-                    System.out.println("Value " + mdr + " added to accumulator.");
+                    alu = (byte) (mdr&0b011111111);
+                    System.out.println("Value " + (mdr&0b011111111) + " added to accumulator.");
                 }
-                alu +=acc;
-                acc = alu;
+
+                alu = (byte) fullAdder(alu,acc);
+                acc=alu;
                 break;
             case 0b0100:
-                alu = acc;
                 if((mdr&0b100000000) == 0b100000000 ){
-                    alu -= MainMemory[mdr&0b011111111];
-                    System.out.println("Subtracted data from memory location " + mdr + " to accumulator.");
+                    alu = (byte) MainMemory[mdr&0b011111111];
+                    System.out.println("Subtracted data from memory location " + (mdr&0b011111111) + " to accumulator.");
                 }
                 else{
-                    alu -= mdr&0b011111111;
-                    System.out.println("Subtracted value " + mdr + " to accumulator.");
+                    alu = (byte) (mdr&0b011111111);
+                    System.out.println("Subtracted value " + (mdr&0b011111111) + " to accumulator.");
                 }
+                alu =Compliment2(alu);
+                alu = fullAdder(alu,acc);
                 acc = alu;
                 break;
             case 0b0101:
-                //Branch to the given data location if the value in the acc is 0
+                if(acc == 0) {
+                    pc = mdr&0b011111111;
+                }
                 break;
             case 0b0110:
+                if(acc>=0){
+                    pc = mdr&0b011111111;
+                }
                 break;
             case 0b0111:
+                pc= mdr&0b011111111;
                 break;
             case 0b1111:
                 System.out.println("OUTPUT: " + acc);
@@ -272,7 +287,26 @@ public class CPU {
         }
         Fetch();
     }
+    public byte fullAdder(int n, int m){
+        int c = 0;//to hold the carry bit
+        int s = 0;//to hold the final added result
+        for(int i = 0; i<8; i++){//loops through all bits in the int
+            int bitmask = 0b1<<i;//creates a bit mask to cycle through each bit
+            int x = n&bitmask;//applies the bit mask to the alu and acc values
+            int y = m&bitmask;
+            int sum = x^y^c;//calculates the sum using XOR
+            s = s | sum;//concatenates the bit to the final result using OR
+            c= (y&(x^c) | x&c)<<1;//calculates the carry bit and shifts it by one for the next calculation
+        }
+        return (byte) s;
+    }
+
+    public byte Compliment2(int x){
+        return (byte) (~x +1);
+    }
 }
+
+
 
 //PROBLEM: CANNOT IMMEDIATE ACCESS AS CPU ASSUMES DIRECT ADDRESSING ALWAYS
 //SOLUTION: A WAY OF INDICATING A DIFFERENCE BETWEEN DATA LOCATIONS AND IMMEDIATE DATA
@@ -282,4 +316,14 @@ public class CPU {
 
 //PROBLEM: CANNOT READ BRANCH STATEMENTS OR BRANCHED AREAS OF CODE
 //SOLUTION: INTEGRATE SOME WAY OF CHECKING IF A LINE OF CODE CONTAINS A BRANCH
-//LIKELY SOLUTION: SPLIT THE LOAD TO MEMORY FUNCTION INTO SEPERATE FUNCTION WITH SOME DESIGNED FOR BRANCHED AND SOME FOR UNBRANCHED CODE
+//LIKELY SOLUTION [WORKED]: SPLIT THE LOAD TO MEMORY FUNCTION INTO SEPARATE FUNCTION WITH SOME DESIGNED FOR BRANCHED AND SOME FOR UNBRANCHED CODE
+
+//PROBLEM: NEED TO SUBTRACT TO BINARY VALUES FROM EACH OTHER
+//SOLUTION: CONVERT BOTH BINARY INTEGERS INTO 2'S COMPLIMENT, THEN MAKE THE NUMBER WE ARE TAKING AWAY INTO A NEGATIVE NUMBER, THEN USE THE FULL ADDER FUNCTION
+//LIKELY SOLUTION: CYCLE THROUGH BOTH INTEGERS BITS, CONVERTING THEM INTO ONES COMPLEMENT AND THEN ADDING 1 TO THEM USING THE ADDER FUNCTION, THEN FLIPPING THE INTEGER BRING TAKEN AWAY TO MAKE IT NEGATIVE, THEN PUTTING BOTH BINARIES THROUGH THE ADDER FUNCTION AGAIN
+//SOLUTION AMENDMENT [TOOK FORWARD]: JAVA AUTOMATICALLY SAVES INTS IN 2S COMPLEMENT AND SO  NO CONVERSION TO 2S COMPLIMENT IS NEEDED, ALL I NEED TO DO IS FLIP AND ADD
+
+//PROBLEM: SUBTRACTION NOT WORKING, MOST LIKELY DUE TO NUMBER OF BITS DURING CALCULATIONS
+//SOLUTION: A WAY OF ENSURING THE NUMBER OF BITS DO NOT INTERFERE WITH THE SUBTRACTION
+//LIKELY SOLUTION : USE THE BYTE DATA TYPE TO LIMIT THE COMPUTING TO 8 BITS ONLY
+//LIKELY SOLUTION: ONLY THE NEGATIVE NUMBER NEEDS TO BE CONVERTED USING 2S COMPLEMENT, THEN ADDED
--- src/what.java
+++ src/what.java
@@ -1,9 +1,8 @@
 public class what {
     public static void main(String[] args) {
         byte a = 0b0111;
-        byte b = 0b1010;
-        int c = a<<4 | b;
-        System.out.println(Integer.toBinaryString(c));
+        byte b = (byte) -a;
+        System.out.println(Integer.toBinaryString(b));
 
     }
 }
